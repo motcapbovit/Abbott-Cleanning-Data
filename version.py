1af4730416c65e33baa569697381ade3595d0c7c
motcapbovit <chithanhdang74@gmail.com>
2024-11-10 20:47:15 +0700

    Adjust layout

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # version.py
 
 VERSION = "1.0.0"
-LAST_UPDATED = "2024-11-10 13:46:24"  # This will be updated manually when you push code
+LAST_UPDATED = "2024-11-10 13:47:15"  # This will be updated manually when you push code
--- views/Dashboard.py
+++ views/Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-if st.session_state.df is None:
+if st.session_state.df is None or "df" not in st.session_state:
     st.write("Chua co data cua df")
 else:
     st.write("Da co data roi nhe")
