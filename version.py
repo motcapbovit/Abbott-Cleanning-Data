b554d4a8828215ee913eb011bc49fd5dfd5b11c5
motcapbovit <chithanhdang74@gmail.com>
2024-11-30 01:46:05 +0700

    Update version 1.0.3

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # version.py
 
-VERSION = "1.0.2"
-LAST_UPDATED = "2024-11-29 18:43:24"  # This will be updated manually when you push code
+VERSION = "1.0.3"
+LAST_UPDATED = "2024-11-29 18:46:05"  # This will be updated manually when you push code
