7bea69c9b6588da13271d0d1f60322cb0b9f3227
motcapbovit <chithanhdang74@gmail.com>
2024-12-01 22:01:55 +0700

    Add outlier for Similac 5G

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # version.py
 
 VERSION = "1.0.3"
-LAST_UPDATED = "2024-12-01 15:00:51"  # This will be updated manually when you push code
+LAST_UPDATED = "2024-12-01 15:01:55"  # This will be updated manually when you push code
