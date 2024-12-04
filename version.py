6117058f81366ef2876f61469a65333200ffb1ff
motcapbovit <chithanhdang74@gmail.com>
2024-12-05 00:55:00 +0700

    Edit README and add load xlsx file feature

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # version.py
 
-VERSION = "1.0.3"
-LAST_UPDATED = "2024-12-04 17:54:30"  # This will be updated manually when you push code
+VERSION = "1.0.4"
+LAST_UPDATED = "2024-12-04 17:55:00"  # This will be updated manually when you push code
